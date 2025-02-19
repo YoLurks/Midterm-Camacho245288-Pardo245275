3da86769379446219d0aee90004c598b1378e91e
YoLurks <lancecamacho29@gmail.com>
2025-02-19 13:01:15 +0800

    initialize the files

---
+++ SceneStarter.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class SceneStarter {
+    
+    public void setUpGUI(){
+        int w = 640;
+        int h = 480;
+        JFrame f = new JFrame();
+        f.setSize(w,h);
+        f.setTitle("Midterm Project - Camacho245288 - Pardo246268");
+        
+    }
+
+    
+}
