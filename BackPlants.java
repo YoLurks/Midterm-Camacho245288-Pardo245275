1f30a8cd647280c7bfac212cc259036cda839d58
YoLurks <lancecamacho29@gmail.com>
2025-02-28 20:30:39 +0800

    edited animation

--- BackPlants.java
+++ BackPlants.java
@@ -1,54 +1,57 @@
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
-public class BackPlants extends DrawingObject {
-    private ArrayList<DrawingObject> elements;
-    private double y;
-    private double x;
-    public BackPlants(){
-        elements = new ArrayList<>();
-        elements.add(new Line(394.5,363.1,417.2,559.7,new Color(107,173,78), 1));
-        elements.add(new Line(418.1,368,418.1,565.9,new Color(107,173,78), 1));
-        elements.add(new Line(439.2,357.4,395.3,550.3,new Color(107,173,78), 1));
-        elements.add(new Line(453.8,381.9,476.6,578.4,new Color(107,173,78), 1));
-        elements.add(new Line(477.4,386.8,477.5,584.6,new Color(107,173,78), 1));
-        elements.add(new Line(487.9,581.3,495,383.5,new Color(107,173,78), 1));
 
-        elements.add(new Circle(385,329.1,16.2,48.4,new Color(255,222,89), -3.2));
-        elements.add(new Circle(411.5,344,10.6,31.6,new Color(255,222,89), -1.3));
-        elements.add(new Circle(437.2,339.5,10.6,31.6,new Color(255,222,89), 11.7));
-        elements.add(new Circle(444.4,347.8,16.2,48.4,new Color(255,222,89), -3.2));
-        elements.add(new Circle(470.9,362.8,10.6,31.6,new Color(255,222,89), -1.3));
-        elements.add(new Circle(489.7,366.4,10.6,31.6,new Color(255,222,89), 0.9));
-        
+public class BackPlants extends DrawingObject {
+    private ArrayList<DrawingObject> lines;   
+    private ArrayList<Circle> circles;        
+    private double swayOffset; 
+    private double swayAngle; 
+
+    public BackPlants() {
+        lines = new ArrayList<>();
+        circles = new ArrayList<>();
+
+    
+        lines.add(new Line(394.5, 363.1, 417.2, 559.7, new Color(107, 173, 78), 1));
+        lines.add(new Line(418.1, 368, 418.1, 565.9, new Color(107, 173, 78), 1));
+        lines.add(new Line(439.2, 357.4, 395.3, 550.3, new Color(107, 173, 78), 1));
+        lines.add(new Line(453.8, 381.9, 476.6, 578.4, new Color(107, 173, 78), 1));
+        lines.add(new Line(477.4, 386.8, 477.5, 584.6, new Color(107, 173, 78), 1));
+        lines.add(new Line(487.9, 581.3, 495, 383.5, new Color(107, 173, 78), 1));
+
+        circles.add(new Circle(385, 329.1, 16.2, 48.4, new Color(255, 222, 89), -3.2));
+        circles.add(new Circle(411.5, 344, 10.6, 31.6, new Color(255, 222, 89), -1.3));
+        circles.add(new Circle(437.2, 339.5, 10.6, 31.6, new Color(255, 222, 89), 11.7));
+        circles.add(new Circle(444.4, 347.8, 16.2, 48.4, new Color(255, 222, 89), -3.2));
+        circles.add(new Circle(470.9, 362.8, 10.6, 31.6, new Color(255, 222, 89), -1.3));
+        circles.add(new Circle(489.7, 366.4, 10.6, 31.6, new Color(255, 222, 89), 0.9));
+
+        swayOffset = 0;
+        swayAngle = 0;
     }
 
     @Override
     public void draw(Graphics2D g) {
-    Graphics2D g2d = (Graphics2D) g;
-    for (DrawingObject obj : elements) {
-        Graphics2D g2dC = (Graphics2D) g2d.create();
-        obj.draw(g2dC);
-        g2dC.dispose();
-    }
-}
-    @Override
-    public double getY() { 
-        return y;
-    }
-    
-    @Override
-    public void setY(double newY) { 
-        this.y = newY; 
+        for (DrawingObject line : lines) {
+            line.draw(g);
+        }
 
+        for (Circle circle : circles) {
+            Graphics2D g2d = (Graphics2D) g.create();
+            AffineTransform at = new AffineTransform();
+            at.translate(swayOffset, 0); 
+            g2d.transform(at);
+            circle.draw(g2d);
+            g2d.dispose();
+        }
     }
 
-    @Override
-    public double getX(){
-        return x;
-    }
-    
-    @Override
-    public void setX(double newX){
-        this.x = newX;
+    public void updateSway() {
+        swayOffset = Math.sin(Math.toRadians(swayAngle)) * 2; 
+        swayAngle += 3.5; 
+        if (swayAngle >= 360) {
+            swayAngle = 0;
+        }
     }
 }
--- FrontPlants.java
+++ FrontPlants.java
@@ -1,52 +1,55 @@
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
+
 public class FrontPlants extends DrawingObject {
-    private ArrayList<DrawingObject> elements;
-    private double y;
-    private double x;
-    public FrontPlants(){
-        elements = new ArrayList<>();
-        elements.add(new Line(72.6,477.3,107.2,775.6,new Color(107,173,78), 1));
-        elements.add(new Line(108.4,484.8,108.4,785,new Color(107,173,78), 1));
-        elements.add(new Line(140.5,468.6,73.8,761.3,new Color(107,173,78), 1));
+    private ArrayList<DrawingObject> lines;   // Fixed stems
+    private ArrayList<Circle> circles;        // Swaying flowers
+    private double swayOffset; // Small horizontal movement
+    private double swayAngle; // Angle used for sin function
 
-        elements.add(new Circle(57.4,429.8,24.6,73.4,new Color(240,208,80), -3.2));
-        elements.add(new Circle(96.7,454.4,16.5,44.6,new Color(240,208,80), -3.2));
-        elements.add(new Circle(135.1,447.1,16.5,44.6,new Color(240,208,80), 13.5));
+    public FrontPlants() {
+        lines = new ArrayList<>();
+        circles = new ArrayList<>();
 
+        lines.add(new Line(72.6, 477.3, 107.2, 775.6, new Color(107, 173, 78), 1));
+        lines.add(new Line(108.4, 484.8, 108.4, 785, new Color(107, 173, 78), 1));
+        lines.add(new Line(140.5, 468.6, 73.8, 761.3, new Color(107, 173, 78), 1));
 
-        elements.add(new Circle(58.3,425.7,24.6,73.4,new Color(255,222,89), -3.2));
-        elements.add(new Circle(98.5,448.3,16.1,47.9,new Color(255,222,89), -1.3));
-        elements.add(new Circle(137.5,441.5,16.1,47.9,new Color(255,222,89), 11.7));
+        circles.add(new Circle(57.4, 429.8, 24.6, 73.4, new Color(240, 208, 80), -3.2));
+        circles.add(new Circle(96.7, 454.4, 16.5, 44.6, new Color(240, 208, 80), -3.2));
+        circles.add(new Circle(135.1, 447.1, 16.5, 44.6, new Color(240, 208, 80), 13.5));
 
+        circles.add(new Circle(58.3, 425.7, 24.6, 73.4, new Color(255, 222, 89), -3.2));
+        circles.add(new Circle(98.5, 448.3, 16.1, 47.9, new Color(255, 222, 89), -1.3));
+        circles.add(new Circle(137.5, 441.5, 16.1, 47.9, new Color(255, 222, 89), 11.7));
+
+        swayOffset = 0; 
+        swayAngle = 0;
     }
 
     @Override
     public void draw(Graphics2D g) {
-    Graphics2D g2d = (Graphics2D) g;
-    for (DrawingObject obj : elements) {
-        Graphics2D g2dC = (Graphics2D) g2d.create();
-        obj.draw(g2dC);
-        g2dC.dispose();
-    }
-}
-    @Override
-    public double getY() { 
-        return y;
-    }
-    
-    @Override
-    public void setY(double newY) { 
-        this.y = newY; 
+        for (DrawingObject line : lines) {
+            line.draw(g);
+        }
 
+        for (Circle circle : circles) {
+            Graphics2D g2d = (Graphics2D) g.create();
+            
+            AffineTransform at = new AffineTransform();
+            at.translate(swayOffset, 0); 
+            g2d.transform(at);
+            circle.draw(g2d);
+            g2d.dispose();
+        }
     }
 
-    @Override
-    public double getX(){
-        return x;
-    }
-
-    public void setX(double newX){
-        this.x = newX;
+    public void updateSway() {
+        swayOffset = Math.sin(Math.toRadians(swayAngle)) * 2;
+        swayAngle += 3.5; 
+        if (swayAngle >= 360) {
+            swayAngle = 0; 
+        }
     }
 }
