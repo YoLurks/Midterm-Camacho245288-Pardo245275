264cb415b539dc03db6d0df564c7131a67057fed
jjscripts <pardojeromeimportant@gmail.com>
2025-02-28 16:34:43 +0800

    fixed triangles (with vines)

--- Triangle.java
+++ Triangle.java
@@ -1,5 +1,5 @@
 import java.awt.*;
-import java.awt.geom.AffineTransform;
+import java.awt.geom.*;
 
 public class Triangle extends DrawingObject {
     private double x;
@@ -20,13 +20,29 @@ public class Triangle extends DrawingObject {
 
     @Override
     public void draw(Graphics2D g) {
-        int[] xPoints = {(int) x, (int) (x + w), (int) (x + w / 2)};
-        int[] yPoints = {(int) (y + h), (int) (y + h), (int) y};
-        Polygon triangle = new Polygon(xPoints, yPoints, 3);
-        AffineTransform transform = new AffineTransform();
-        transform.rotate(Math.toRadians(rotation), x, y);
-        Shape rotatedTriangle = transform.createTransformedShape(triangle);
+        Path2D triangle = new Path2D.Double();
+    
+        double x1 = x + w / 2;
+        double y1 = y;    
+        double x2 = x;
+        double y2 = y + h;            
+        double x3 = x + w;
+        double y3 = y + h;      
+        
+        triangle.moveTo(x1, y1);
+        triangle.lineTo(x2, y2);
+        triangle.lineTo(x3, y3);
+        triangle.closePath();
+    
 
+        double centerX = (x1 + x2 + x3) / 3;
+        double centerY = (y1 + y2 + y3) / 3;
+    
+        AffineTransform transform = new AffineTransform();
+        
+        transform.rotate(Math.toRadians(rotation), centerX, centerY);
+        Shape rotatedTriangle = transform.createTransformedShape(triangle);
+    
         g.setColor(color);
         g.fill(rotatedTriangle);
     }
